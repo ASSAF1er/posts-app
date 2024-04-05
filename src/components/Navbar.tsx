41a7ef001fe9cc50e9424e8b9e00f10a431948a3
Assaf1er <assaffart@gmail.com>
2024-04-05 21:59:08 +0100

    ajout de la navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import user_profile from "../assets/user_profile.png";
 function Navbar() {
   return (
-    <nav className="flex px-[5%] py-5 justify-between items-center ">
-      <div className="text-3xl font-bold text-gray-950">Posts</div>
-      <div>
+    <nav className="flex px-[5%] py-4 justify-between items-center border-b border-gray-200">
+      <div className="flex text-3xl font-bold text-gray-950">
+        <div className="mr-1">
+          <span className="px-2 rounded-md text-slate-50 bg-gray-950">P</span>
+        </div>
+        <h1>Posts</h1>
+      </div>
+      <div className="flex gap-4 items-center">
+        <span className="material-icons text-gray-600">light_mode</span>
         <img
           src={user_profile}
           alt="user profile"
